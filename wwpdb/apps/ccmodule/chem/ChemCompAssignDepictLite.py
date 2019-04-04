f21b937d34cf528949fc2e24c5977a34261bdb62
Ezra Peisach <ezra.peisach@rcsb.org>
2019-04-04 14:53:57 -0400

    Improve look

--- wwpdb/apps/ccmodule/chem/ChemCompAssignDepictLite.py
+++ wwpdb/apps/ccmodule/chem/ChemCompAssignDepictLite.py
@@ -323,10 +323,7 @@ class ChemCompAssignDepictLite(ChemCompDepict):
 
         # Artifical row to select "none" as "focus of research
         oL.append('<tr class="%s c_NONE_special">' % self.__rowClass(iRow) )
-        oL.append('<td class="">None</td>') # Ligand ID column
-        oL.append('<td class="NONE_plchldr"></td>') # number of instances column
-        oL.append('<td class="NONE_plchldr"></td>') # status column
-        oL.append('<td class="NONE_plchldr"></td>' ) # select for inspection column
+        oL.append('<td class="" colspan=4>No ligand as focus of research</td>') 
         oL.append('<td class="selectinstnc_td"><input name="NONE%%" id="NONE_rsrch" type="checkbox" class="selectinstnc_rsrch selectinst_none" %s %s></td>' % (None_checked, None_check_disabled) )
         oL.append('</tr>')
         iRow+=1
