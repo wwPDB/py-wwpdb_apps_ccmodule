f554341ec8815c8db5ee673c131238e9f992f192
Ezra Peisach <ezra.peisach@rcsb.org>
2019-08-24 22:10:59 -0400

    Python 3: octal handling, has_key, open a file binary, handle string types

--- wwpdb/apps/ccmodule/webapp/ChemCompWebApp.py
+++ wwpdb/apps/ccmodule/webapp/ChemCompWebApp.py
@@ -361,7 +361,7 @@ class ChemCompWebAppWorker(object):
         """
         #
         reqPath=self.__reqObj.getRequestPath()
-        if not self.__appPathD.has_key(reqPath):
+        if reqPath not in self.__appPathD:
             # bail out if operation is unknown -
             rC=ResponseContent(reqObj=self.__reqObj, verbose=self.__verbose,log=self.__lfh)
             rC.setError(errMsg='Unknown operation')
@@ -381,7 +381,7 @@ class ChemCompWebAppWorker(object):
         #
         try:
             reqPath=self.__reqObj.getRequestPath()
-            if not self.__appPathD.has_key(reqPath):
+            if reqPath not in self.__appPathD::
                 # bail out if operation is unknown -
                 rC=ResponseContent(reqObj=self.__reqObj, verbose=self.__verbose,log=self.__lfh)
                 rC.setError(errMsg='Unknown operation')
@@ -740,7 +740,7 @@ class ChemCompWebAppWorker(object):
             cifObj = mmCIFUtil(filePath=self.__modelFilePath)
             list = cifObj.GetValue('database_2')
             for dir in list:
-                if (not dir.has_key('database_id')) or (not dir.has_key('database_code')):
+                if ('database_id' not in dir) or ('database_code' not in dir):
                     continue
                 #
                 dbname = dir['database_id'].upper()
@@ -752,7 +752,7 @@ class ChemCompWebAppWorker(object):
             #
             list = cifObj.GetValue('struct')
             for dir in list:
-                if( not dir.has_key('title') ):
+                if 'title' not in dir:
                     continue
                 #
                 entryTitle = dir['title']
